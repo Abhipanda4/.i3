f2b2770a856404b6d9d5b2d40f660ecd4f272e0d
abhipanda4 <abhipanda03@gmail.com>
2017-01-03 21:52:24 +0530

    My i3-setup.

---
+++ screencast.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#gnome-screenshot -d 0 -f ~/Pictures/fori3lock_temp.png
+scrot 'fori3lock.png' -e 'mv $f ~/.config/i3/'
+#convert -blur 0x6 -modulate 150,150 ~/Pictures/fori3lock_temp.png ~/Pictures/fori3lock.png
+convert -blur 0x6 ~/.config/i3/fori3lock.png ~/.config/i3/final.png
+i3lock -i ~/.config/i3/final.png -e -f
