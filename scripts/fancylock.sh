f0bbe8972ec00a2901873cedd75c10782165c4e3
abhipanda4 <abhipanda03@gmail.com>
2018-01-22 15:55:01 +0530

    i3lock color added

---
+++ scripts/fancylock.sh
@@ -0,0 +1,8 @@
+# /bin/bash
+
+i3lock -n -i ~/.i3/pics/out.png\
+    --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
+    --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
+    --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
+    --ringvercolor=ffffffff --ringwrongcolor=ffffffff --indpos="x+96:y+1000" \
+    --radius=45 --veriftext="" --wrongtext=""
